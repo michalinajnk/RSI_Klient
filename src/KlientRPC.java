85745e1b6dba94ebcf8114d52ab5df4680ea6476
MrTitainin <jaromirb@op.pl>
2022-03-13 23:40:47 +0100

    2-Machine preparations

--- src/KlientRPC.java
+++ src/KlientRPC.java
@@ -2,22 +2,24 @@
 import org.apache.xmlrpc.XmlRpcClient;
 
 import java.util.Vector;
+import java.util.regex.Pattern;
 
 public class KlientRPC {
 
     public static void main(String[] args) {
         try {
-            XmlRpcClient srv = new XmlRpcClient("http://localhost:10005");
+            String address = args.length>0 && Pattern.matches("\\d+\\.\\d+\\.\\d+\\.\\d+",args[0])?args[0]:"localhost";//if first argument doesn't seem like an address, use localhost
+            XmlRpcClient srv = new XmlRpcClient(String.format("http://%s:10005",address));
             Vector<Integer> params = new Vector<Integer>();
-            params.addElement(new Integer(13));
-            params.addElement(new Integer(21));
+            params.addElement(Integer.valueOf(13));
+            params.addElement(Integer.valueOf(21));
             Object result = srv.execute("MojSerwer.echo", params);
             int wynik = ((Integer) result).intValue();
             System.out.println("wynik to:" + wynik);
             AC cb = new AC();
 
             Vector<Integer> params2 = new Vector<Integer>();
-            params2.addElement(new Integer(3000));
+            params2.addElement(Integer.valueOf(3000));
             srv.executeAsync("MojSerwer.execAsy", params2, cb);
             System.out.println("Wywolano asynchronicznie");
 
