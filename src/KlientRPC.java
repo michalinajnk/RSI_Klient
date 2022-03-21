c8592c77463b7ff1dd793159fe44f18e3f3fb30c
michalinajnk <254564@student.pwr.edu.pl>
2022-03-21 09:03:15 +0100

    n

--- src/KlientRPC.java
+++ src/KlientRPC.java
@@ -1,4 +1,3 @@
-
 import org.apache.xmlrpc.XmlRpcClient;
 
 import java.util.Vector;
@@ -6,8 +5,11 @@ import java.util.regex.Pattern;
 
 public class KlientRPC {
 
+
     public static void main(String[] args) {
         try {
+
+
             String address = args.length>0 && Pattern.matches("\\d+\\.\\d+\\.\\d+\\.\\d+",args[0])?args[0]:"localhost";//if first argument doesn't seem like an address, use localhost
             XmlRpcClient srv = new XmlRpcClient(String.format("http://%s:10005",address));
             Vector<Integer> params = new Vector<Integer>();
@@ -24,14 +26,33 @@ public class KlientRPC {
             System.out.println("Wywolano asynchronicznie");
 
 
+            System.out.println("Is the task done?: ");
+
+
+
+            Vector<Object> params3 = new Vector<Object>();
+
+            params3.addElement("c");
+            params3.addElement("it has been already done");
+            params3.addElement( 1.5);
+            params3.addElement( 1);
+            Object result2 = (String) srv.execute("MojSerwer.handleTask", params3);
+            System.out.println(result2.toString());
 
 
         } catch (Exception exception) {
-            System.err.println("Klient XML-RPC: " + exception);
+            System.err.println("Klient XML-RPC: " + exception.getLocalizedMessage() + ", "+ exception.getCause() + ", "+ exception.getMessage() + ", " + exception.getClass() );
         }
 
 
     }
+
+
+
+
+
+
+
 }
 
 
